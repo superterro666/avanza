7c768e42c0344cd33c94972f054cca6602cc60a3
superterro666 <superterro92@gmail.com>
2017-11-05 17:39:36 +0100

    blog foto  en proceso

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { SimpleTinyComponent } from './simple-tiny.component';
 import { NgForm } from '@angular/forms';
 import { BlogService } from '../servicios/blog.service';
+import { UploadService } from '../servicios/upload.service';
 
 
 @Component({
@@ -12,14 +13,16 @@ import { BlogService } from '../servicios/blog.service';
 })
 
 export class BlogComponent implements OnInit {
+  public filesToUpload: Array<File>;
   public texto: string;
   private blogs: any = [];
   public blog = {
     id: 0,
     titulo: '',
-    texto: ''
+    texto: '',
+    imagen: ''
  };
-  constructor(private _blog: BlogService, private _tiny: SimpleTinyComponent) {
+  constructor(private _blog: BlogService, private _tiny: SimpleTinyComponent, private _upload: UploadService) {
     this.getBlogs();
    }
 
@@ -43,6 +46,12 @@ export class BlogComponent implements OnInit {
 
       this._blog.listaBlog$.subscribe(data => {
       this.blogs = data;
+      this._upload.makeFileRequest(this.blogs[0]['id'], this.filesToUpload, 'imagen', 'blog')
+      .then((result: any) => {
+        this.blogs[0]['imagen'] = result;
+        }, error => {
+         console.log(error);
+       });
       });
     }
   }
@@ -90,5 +99,10 @@ export class BlogComponent implements OnInit {
    });
 
  }
+
+ fileChangeEvent(fileinput: any) {
+  this.filesToUpload = <Array<File>>fileinput.target.files;
+  console.log(this.filesToUpload);
+}
 
 }
